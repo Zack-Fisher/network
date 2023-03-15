55fe1a0a0b5a765c8697bf7d9bdb4f6cd5c03d05
Zack-Fisher <zackfisher27@proton.me>
2023-03-15 19:47:35 -0400

    tagging and analysis event systems

--- src/level_instantiation/spawning/objects/button.rs
+++ src/level_instantiation/spawning/objects/button.rs
@@ -3,19 +3,19 @@ use bevy::prelude::*;
 use bevy_rapier3d::prelude::Collider;
 use serde::{Serialize, Deserialize};
 
-use crate::world_interaction::analysis::{AnalyseBundle, Analysis, AnalysisEventType, AnalysisEvent};
+use crate::world_interaction::analysis::{AnalyseBundle, Analysis, AnalysisEventType, AnalysisEvent, event::EventTypes};
 
 //primitive shapes to help with level design
 #[derive(Default, Component, Reflect, Serialize, Deserialize, Debug)]
 #[reflect(Component, Serialize, Deserialize)]
-pub struct ButtonPrefab {
-    pub payload: String,
+pub struct AnalysisPrefab {
+    pub analysis: Analysis
 }
 
-pub fn build_button (
+pub fn build_analysis (
     mut commands: Commands,
 
-    prefab_q: Query<(Entity, &ButtonPrefab), Added<ButtonPrefab>>,
+    prefab_q: Query<(Entity, &AnalysisPrefab), Added<AnalysisPrefab>>,
 
     mut meshes: ResMut<Assets<Mesh>>,
     mut mats: ResMut<Assets<StandardMaterial>>,
@@ -38,13 +38,10 @@ pub fn build_button (
                                 }),
                                 ..default()
                             },
-                            analysis: Analysis {
-                                event: Some(AnalysisEvent {
+                            analysis: Analysis(Some(AnalysisEvent {
                                     payload: prefab.payload.clone(),
                                     ev_type: AnalysisEventType::Button,
-                                }),
-                                ..default()
-                            },
+                                })),
                             ..default()
                         }
                     )
